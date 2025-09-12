6d49275ac0be5101b4ba59b71cae66b88d205865
waxberry <chenyiming_hust@163.com>
2025-09-12 19:48:29 +0800

    PA2.2: tests/string.c pass

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -4,6 +4,7 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+// exclude '\0'
 size_t strlen(const char *s) {
   size_t i; 
   for (i=0; s[i]!='\0'; i++); 
@@ -25,7 +26,13 @@ char *strncpy(char *dst, const char *src, size_t n) {
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  size_t dst_len = strlen(dst); 
+  size_t i; 
+  for(i=0; src[i]!='\0'; i++) {
+    dst[dst_len+i] = src[i]; 
+  }
+  dst[dst_len+i]='\0';
+  return dst;
 }
 
 /*
@@ -55,7 +62,12 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  unsigned char *p = s; 
+  size_t i; 
+  for(i=0; i<n; i++) {
+    p[i] = c; // why here p cant be const?
+  }
+  return p;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
@@ -67,7 +79,16 @@ void *memcpy(void *out, const void *in, size_t n) {
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  // 需要转换为纯粹的无符号字节比较
+  const unsigned char *p1 = s1; // must initialization
+  const unsigned char *p2 = s2;  
+  while(n>0) {
+    if (*p1 != *p2) return *p1 - *p2; 
+    p1++; // why const can change?
+    p2++;
+    n--;
+  }
+  return 0; 
 }
 
 #endif
