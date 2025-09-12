63dafa695baf478d2341cc2e6dcd3c8442288724
waxberry <chenyiming_hust@163.com>
2025-09-12 12:43:51 +0800

    AMstring.c: temp

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,27 +5,53 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  panic("Not implemented");
+  size_t i; 
+  for (i=0; s[i]!='\0'; i++); 
+  return i;
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  size_t i;
+  for (i=0; src[i] != '\0'; i++) dst[i] = src[i];
+  dst[i] = '\0'; 
+  return dst;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+  size_t i; 
+  for (i=0; i<n&&src[i]!='\0'; i++) dst[i] = src[i]; 
+  for (; i<n; i++) dst[i] = '\0';
+  return dst;
 }
 
 char *strcat(char *dst, const char *src) {
   panic("Not implemented");
 }
 
+/*
+  Compare s1 and s2 by byte
+  return a integer
+    >0, s1>s2
+    =0, s1=s2
+    <0, s1<s2
+*/
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  while (*s1&&*s1==*s2) {
+    s1++;
+    s2++;
+  }
+  return *(const unsigned char*)s1 - *(const unsigned char*)s2;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+  if (n==0) {
+    return 0;
+  }
+  while (*s1 && *s1==*s2 && --n) {
+    s1++; 
+    s2++;
+  }
+  return *(const unsigned char*)s1 - *(const unsigned char*)s2;
 }
 
 void *memset(void *s, int c, size_t n) {
