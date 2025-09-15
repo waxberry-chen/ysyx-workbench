4b7de479705406cb3acd46c5a1803732d14a5436
waxberry <chenyiming_hust@163.com>
2025-09-15 11:52:07 +0800

    PA2.2: hello-str pass

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -15,7 +15,42 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 
 // Write to a character string
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  int d;
+  char c, *s, *dst_ptr;
+  dst_ptr = out;
+  va_start(ap, fmt);
+  while (*fmt) {
+    if (*fmt == '%') {
+       switch(*++fmt) {
+        case 's':
+          s = va_arg(ap, char *);
+          strcat(dst_ptr, s);
+          dst_ptr += strlen(s);
+          fmt++;
+          break;
+        case 'c':
+          c = va_arg(ap, int); // no char here
+          *dst_ptr = c;
+          dst_ptr++;
+          fmt++;
+          break;
+        case 'd':
+          d = va_arg(ap, int);
+          int d_len = my_itoa(dst_ptr, d);
+          dst_ptr += d_len;
+          fmt++;
+          break;
+       }
+    } else {
+      *dst_ptr = *fmt;
+      dst_ptr++;
+      fmt++;
+    }
+  }
+  *dst_ptr = '\0';
+  va_end(ap);
+  return (int)strlen(out);
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
@@ -24,6 +59,26 @@ int snprintf(char *out, size_t n, const char *fmt, ...) {
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
   panic("Not implemented");
+}
+
+// integer to ascii - my_itoa
+int my_itoa(char *dst, int d) {
+  int i=0, j=0;
+  while (d) {
+    dst[i++] = d%10 + '0';
+    d /= 10; 
+  }
+  while (j < i/2) {
+    int temp = dst[j]; 
+    dst[j] = dst[i-j-1]; 
+    dst[i-j-1] = temp;
+    j++;
+  }
+  if (i==0) {
+    dst[i++] = '0';
+  }
+  dst[i]='\0';
+  return i;
 }
 
 #endif
